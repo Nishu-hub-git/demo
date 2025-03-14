fb7354cac243aa2b0fe4bcf3bb73762465722f21
Nishu_hub_git <bjnishchitha@gmail.com>
2025-03-14 13:26:36 +0530

    project 8

---
+++ demo/src/main/java/com/Nishu_hub_git/App.java
@@ -0,0 +1,13 @@
+package com.Nishu_hub_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
